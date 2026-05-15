9a076c2173b57da629963aee8201f15c95bf887d
Mathiu21-Gomez <mathiugomez19@gmail.com>
2026-05-15 13:10:07 -0400

    test: add global teardown for DOM and localStorage in Vitest setup

--- shared/testing/setup.ts
+++ shared/testing/setup.ts
@@ -2,4 +2,7 @@ import { afterEach } from 'vitest'
 
 afterEach(() => {
   document.body.innerHTML = ''
+  if (typeof window !== 'undefined' && window.localStorage) {
+    window.localStorage.clear()
+  }
 })
